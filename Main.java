80a44e33692c3f6b2a3cdb315d1f92759e1e7aa7
BOUZDUG Zakaria <zakariabouzdig@gmail.com>
2025-03-14 03:37:03 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 package test;
-
+import beans.*;
+import services.GestionCandidatures;
 public class Main {
+    public static void main(String[] args) {
+        GestionCandidatures gestion = new GestionCandidatures();
 
+        Master master1 = new Master(1, "Informatique", "Sciences", 30);
+        gestion.ajouterMaster(master1);
+
+        Candidat candidat1 = new Candidat(1, "Dupont", "Jean", "Licence Informatique", "Bien");
+        Candidat candidat2 = new Candidat(2, "Martin", "Sophie", "Licence Mathématiques", "Très Bien");
+        Candidature candidature1 = new Candidature(1, master1.getId(), candidat1.getId(), "En attente");
+        gestion.enregistrerCandidature(candidature1);
+
+        Candidat recherche = gestion.rechercherCandidat("Dupont");
+        if (recherche != null) {
+            System.out.println("Candidat trouvé: " + recherche.getNom() + " " + recherche.getPrenom());
+        } else {
+            System.out.println("Candidat non trouvé.");
+        }
+    }
 }
